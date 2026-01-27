acab0499f4ae06796dc5f140ec4f896fa06b57bd
MansourM61 <22415290+MansourM61@users.noreply.github.com>
2026-01-27 21:57:32 +0000

    Tailwindcss, data fetching, and reactivity added at the client side.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface Data {
+  id: number;
+  data: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<Data | null>(null);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const rawData = await fetch("/api");
+      const jsonData = await rawData.json();
+
+      setData(jsonData);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl text-center text-cyan-700">
+        {data !== null ? data.id : "fetching"}
+      </h1>
+      <h1 className="mt-10 text-4xl text-center text-red-900">
+        {data !== null ? data.data : "fetching"}
+      </h1>
+
+      <button
+        className="border-2 border-black bg-green-900 text-2xl text-center block mx-auto"
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        Click me, counter = {count}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

759ed336e86ddb844c3008da64c04cf243489f17
MansourM61 <22415290+MansourM61@users.noreply.github.com>
2026-01-27 22:36:28 +0000

    Client build output added as root for static serving.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
 import * as extra from "./routes/extra"
 import * as api from "./apis/api";
+import { serveStatic } from 'hono/bun';
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.use("/*", serveStatic({ root: "../client/dist" }))
+
+app.get('/about', (c) => {
     return c.text('Hono: landing page!')
 })
 
@@ -12,6 +15,6 @@ app.route("/extra", extra.default);
 app.route("/api", api.default);
 
 export default {
-    port: 8080,
+    port: 3000,
     fetch: app.fetch
 }
